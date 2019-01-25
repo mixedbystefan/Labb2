13a83ef075954864099e9ed4dc7ddcb85468c0d8
Stefan Sundström <mixedbystefan@gmail.com>
2019-01-25 11:23:15 +0100

    Changed name on Class

--- Hyresforeningen/src/filmservice/ApartmentService.java
+++ Hyresforeningen/src/filmservice/ApartmentService.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 @Path("/ApartmentService")
-public class FilmService {
+public class ApartmentService {
 	DBUtility dBUtility = new DBUtility();
    @GET
    @Path("/apartments")
