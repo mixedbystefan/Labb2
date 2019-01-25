d0aa4c5a0db0f737412eaefdbe05e60449416a45
Stefan Sundström <mixedbystefan@gmail.com>
2019-01-25 11:21:03 +0100

    Nu funkar det med klassen films - ska byta till lista av apartments

--- Hyresforeningen/src/filmservice/Film.java
+++ Hyresforeningen/src/filmservice/Film.java
@@ -15,13 +15,17 @@ public class Film {
 	public String getDescription() {return description;}
 	@Override
 	public String toString() {return id + " " + title + "\n" + description;}
-	@Override
-	public boolean equals(Object object){
-      if(object == null){return false;}
-	else if(!(object instanceof Film)){return false;}
-	else {
-    	 Film film = (Film)object;
-       if(id == film.getId() && title.equals(film.getTitle()) && description.equals(film.getDescription())){
-            return true;}}
-      return false;
-   }   }
+	
+//	@Override
+//	public boolean equals(Object object)
+//	{
+//		if(object == null){return false;}
+//		else if(!(object instanceof Film)){return false;}
+//		else { 
+//			Film film = (Film)object;
+//			if(id == film.getId() && title.equals(film.getTitle()) && description.equals(film.getDescription()))
+//			{return true;}
+//			}
+//      return false;
+//   }   
+	}
--- Hyresforeningen/src/filmservice/FilmService.java
+++ Hyresforeningen/src/filmservice/FilmService.java
@@ -5,15 +5,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-@Path("/FilmService")
+@Path("/ApartmentService")
 public class FilmService {
+	DBUtility dBUtility = new DBUtility();
    @GET
-   @Path("/films")
+   @Path("/apartments")
    @Produces(MediaType.APPLICATION_JSON)
-   public List<Film> getFilms() {
+   public List<Film> getFilms() throws Exception {
 	   List<Film> out = new ArrayList<Film>();
 	   out.add(new Film("id0", "Modern Times", "En komedi-klassiker om arbetares kamp mot teknik och kapitalism."));
 	   out.add(new Film("id1", "Metropolis", "En science fiction-klassiker om arbetares kamp mot teknik och kapitalism."));
 	   return out;
+	   //System.out.println("hej");
+	   //List<Apartment> Apartments = dBUtility.getAllApartments();
+	   //System.out.println(Apartments);
+	   //return Apartments;
+	   
    }
 }
+
+// Det funkar nu , måste få det att fungera med Aparments
